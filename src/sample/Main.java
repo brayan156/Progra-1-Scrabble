1c7abeff17e6bc54bc12637178fe201520883ca3
Brayan León U <44703877+brayan156@users.noreply.github.com>
2021-09-29 20:55:22 -0600

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -29,7 +29,8 @@ public class Main extends Application {
 
     public void renovar_fichas_en_pantalla(ListaFichas fichas){ //recibe la lista que envia el server de las fichas que el cliente no uso
         elminar_fichas_en_pantalla();
-        pintar_nuevas_fichas_en_pantalla(fichas,14,701);
+        configurar_parametros_ficha(fichas,14,701);
+        pintar_nuevas_fichas_en_pantalla(fichas);
         this.listaFichas=fichas;
     }
 
@@ -40,29 +41,32 @@ public class Main extends Application {
         }
     }
 
-    public void pintar_nuevas_fichas_en_pantalla(ListaFichas listaNuevasFichas,int pocisionRelativaX, int pocisionRelativaY) {
+    public void pintar_nuevas_fichas_en_pantalla(ListaFichas listaNuevasFichas) {
         int cantidadFichas= listaNuevasFichas.getLargo();
-        distancia_entre_fichas=41;
-        int posicionX= pocisionRelativaX, pocisionY = pocisionRelativaY;
-        for (int i=0; i<cantidadFichas; i++) {//aqui se pintan las imagenes nuevas
+        for (int i=0; i<cantidadFichas; i++) { //aqui se pintan las imagenes nuevas
             Ficha fichatmp= listaNuevasFichas.buscar(i);
-            configurar_parametros_ficha(fichatmp,posicionX,pocisionY);
             juegopane.getChildren().add(fichatmp);
-            posicionX+=distancia_entre_fichas;
         }
     }
 
-    public void configurar_parametros_ficha(Ficha ficha, int posicion_x, int posicion_y){
-        double altura_ficha=41 , ancho_ficha=41;
-        ficha.crearimagen();
-        ficha.setFitHeight(altura_ficha);
-        ficha.setFitWidth(ancho_ficha);
-        ficha.setPosx(posicion_x);
-        ficha.setPosy(posicion_y);
-        ficha.setOnMousePressed(pressear);
-        ficha.setOnMouseDragged(draggear);
-        ficha.setOnMouseReleased(quitarclick);
-    }   
+    public void configurar_parametros_ficha(ListaFichas listaNuevasFichas, int pocisionRelativaX, int pocisionRelativaY){
+        int cantidadFichas= listaNuevasFichas.getLargo();
+        double altura_ficha = 41 , ancho_ficha = 41;
+        int distancia_entre_fichas=41;
+        int posicionX = pocisionRelativaX, pocisionY = pocisionRelativaY;
+        for (int i=0; i<cantidadFichas; i++) {
+            Ficha ficha= listaNuevasFichas.buscar(i);
+            ficha.crearimagen();
+            ficha.setFitHeight(altura_ficha);
+            ficha.setFitWidth(ancho_ficha);
+            ficha.setPosx(posicionX);
+            ficha.setPosy(pocisionY);
+            ficha.setOnMousePressed(pressear);
+            ficha.setOnMouseDragged(draggear);
+            ficha.setOnMouseReleased(quitarclick);
+            posicionX+=distancia_entre_fichas;
+        }
+    }
     
  
     
