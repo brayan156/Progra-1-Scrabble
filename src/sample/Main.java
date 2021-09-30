b64fb051c2443b7c2d94cfe1487aca53b52d97cf
Brayan León U <44703877+brayan156@users.noreply.github.com>
2021-09-29 21:44:02 -0600

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -27,49 +27,5 @@ public class Main extends Application {
     }
     
 
-    public void renovar_fichas_en_pantalla(ListaFichas fichas){ //recibe la lista que envia el server de las fichas que el cliente no uso
-        elminar_fichas_en_pantalla();
-        configurar_parametros_ficha(fichas,14,701);
-        pintar_nuevas_fichas_en_pantalla(fichas);
-        this.listaFichas=fichas;
-    }
-
-    public void elminar_fichas_en_pantalla() {
-        int largo_lista_fichas_en_pantalla = this.listaFichas.getLargo();
-        for (int i = 0; i < largo_lista_fichas_en_pantalla; i++) {//aqui se eliminan la imagenes viejas
-            juegopane.getChildren().remove(this.listaFichas.buscar(i));
-        }
-    }
-
-    public void pintar_nuevas_fichas_en_pantalla(ListaFichas listaNuevasFichas) {
-        int cantidadFichas= listaNuevasFichas.getLargo();
-        for (int i=0; i<cantidadFichas; i++) { //aqui se pintan las imagenes nuevas
-            Ficha fichatmp= listaNuevasFichas.buscar(i);
-            juegopane.getChildren().add(fichatmp);
-        }
-    }
-
-    public void configurar_parametros_ficha(ListaFichas listaNuevasFichas, int pocisionRelativaX, int pocisionRelativaY){
-        int cantidadFichas= listaNuevasFichas.getLargo();
-        double altura_ficha = 41 , ancho_ficha = 41;
-        int distancia_entre_fichas=41;
-        int posicionX = pocisionRelativaX, pocisionY = pocisionRelativaY;
-        for (int i=0; i<cantidadFichas; i++) {
-            Ficha ficha= listaNuevasFichas.buscar(i);
-            ficha.crearimagen();
-            ficha.setFitHeight(altura_ficha);
-            ficha.setFitWidth(ancho_ficha);
-            ficha.setPosx(posicionX);
-            ficha.setPosy(pocisionY);
-            ficha.setOnMousePressed(pressear);
-            ficha.setOnMouseDragged(draggear);
-            ficha.setOnMouseReleased(quitarclick);
-            posicionX+=distancia_entre_fichas;
-        }
-    }
-    
- 
-    
-
 }
 
